govtrack/django-simplegetapi

commit bbb4b5e2f4974e30da3091bf4bb9d784465daf47

make enum handling work with python 3 enums as model field choices too

simplegetapi/utils.py
@@ -7,16 +7,42 @@ if "unicode" not in globals():
 try:
     from common import enum as enummodule
     has_common_enum = True
-except:
+except ImportError:
     has_common_enum = False
+
+try:
+    import enum
+    has_enum = True
+except ImportError:
+    has_enum = False
 
 from django.db.models.related import RelatedObject
 
-def is_enum(obj):
-    if not has_common_enum:
-        return False
-    return inspect.isclass(obj) and issubclass(obj, enummodule.Enum)
-    
+def is_enum(choices):
+    return is_enum_pyenum(choices) or is_enum_commonenum(choices)
+
+def is_enum_pyenum(choices):
+    return has_enum and len(choices) > 0 and isinstance(choices[0][0], enum.Enum)
+
+def is_enum_commonenum(choices):
+    return has_common_enum and inspect.isclass(choices) and issubclass(obj, enummodule.Enum)
+
+def enum_key_to_value(enumclass, key):
+    if is_enum_commonenum(enumclass):
+        return int(enumclass.by_key(v))
+
+def enum_get_values(choices):
+    if is_enum_commonenum(choices):
+        dict((v.key, { "label": v.label, "description": getattr(v, "search_help_text", None) } ) for v in enum.values())
+    if is_enum_pyenum(choices):
+        return { k.name: { "label": k.name, "description": "" } for k, v in choices }
+
+def enum_value_to_key_and_label(choices, value):
+    if is_enum_commonenum(choices):
+        return choices.by_value(value).key, choices.by_value(value).label
+    if is_enum_pyenum(choices):
+        return (value.name, None)
+
 def get_orm_fields(obj):
     for field in obj._meta.fields + \
         obj._meta.many_to_many + \

simplegetapi/views.py
@@ -8,7 +8,7 @@ from django.conf import settings
 import csv, json, datetime, lxml, urllib
 import dateutil.parser
 
-from simplegetapi.utils import is_enum, get_orm_fields
+from simplegetapi.utils import is_enum, enum_key_to_value, enum_get_values, get_orm_fields
 from simplegetapi.serializers import serialize_object, serialize_response_json, serialize_response_jsonp, serialize_response_xml, serialize_response_csv
 
 def get_api_models():
@@ -307,7 +307,7 @@ def normalize_field_value(v, model, modelfield):
     if choices and is_enum(choices):
         try:
             # Convert the string value to the raw database integer value.
-            return int(choices.by_key(v))
+            return enum_key_to_value(choices, v)
         except: # field is not a model field, or enum value is invalid (leave as original)
             raise ValueError("%s is not a valid value; possibly values are %s" % (v, ", ".join(c.key for c in choices.values())))
 
@@ -461,7 +461,7 @@ def build_api_documentation(model, qs):
             # Choices?
             enum = field.choices
             if is_enum(enum):
-                field_info["enum_values"] = dict((v.key, { "label": v.label, "description": getattr(v, "search_help_text", None) } ) for v in enum.values())
+                field_info["enum_values"] = enum_get_values(enum)
 
         # Stupid Django hard-coded text.
         field_info["help_text"] = field_info.get("help_text", "").replace('Hold down "Control", or "Command" on a Mac, to select more than one.', '')

simplegetapi/serializers.py
@@ -14,7 +14,7 @@ from django.db.models import Model
 from django.db.models.fields.related import ForeignKey, ManyToManyField
 from django.db.models.related import RelatedObject
 
-from simplegetapi.utils import is_enum, get_orm_fields
+from simplegetapi.utils import is_enum, enum_value_to_key_and_label, get_orm_fields
 
 def serialize_object(obj, recurse_on=[], requested_fields=None):
     """Serializes a Python object to JSON-able data types (listed in the 1st if block below)."""
@@ -110,13 +110,15 @@ def serialize_object(obj, recurse_on=[], requested_fields=None):
                 
             # For enumerations, output the key and label and not the raw database value.
             elif v is not None and is_enum(choices):
-                v = choices.by_value(v)
-                ret[field_name] = v.key
-                ret[field_name + "_label"] = v.label
+                key, label = enum_value_to_key_and_label(choices, v)
+                ret[field_name] = key
+                if label:
+                    ret[field_name + "_label"] = label
                 
             # For all other values, serialize by recursion.
             else:
                 ret[field_name] = serialize_object(v, recurse_on=sub_recurse_on, requested_fields=sub_fields)
+                
         return ret
         
     # For all other object types, convert to unicode.

commit 8b96098ed7ad9fb3bc935e527539551b9c61a9fb

fix for enum handling broken by bbb4b5e2f4974e30da3091bf4bb9d784465daf47

simplegetapi/utils.py
@@ -29,7 +29,7 @@ def is_enum_commonenum(choices):
 
 def enum_key_to_value(enumclass, key):
     if is_enum_commonenum(enumclass):
-        return int(enumclass.by_key(v))
+        return int(enumclass.by_key(key))
 
 def enum_get_values(choices):
     if is_enum_commonenum(choices):

simplegetapi/views.py
@@ -323,7 +323,7 @@ def normalize_field_value(v, model, modelfield):
             # Convert the string value to the raw database integer value.
             return enum_key_to_value(choices, v)
         except: # field is not a model field, or enum value is invalid (leave as original)
-            raise ValueError("%s is not a valid value; possibly values are %s" % (v, ", ".join(c.key for c in choices.values())))
+            raise ValueError("%s is not a valid value; possible values are %s" % (v, ", ".join(c.key for c in choices.values())))
 
     # If this is a filter on a datetime field, parse the date in ISO format
     # because that's how we serialize it. Normally you can just pass a string