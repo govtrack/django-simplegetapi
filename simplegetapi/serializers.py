govtrack/django-simplegetapi

commit 4ed6eae5e695845e336632239b6c915ffe9ae1ac

Python 3.x compatibility

simplegetapi/serializers.py
@@ -1,4 +1,13 @@
-import StringIO, datetime, json, csv, lxml.etree
+import datetime, json, csv, lxml.etree
+
+try:
+    # Python 2.x
+    from StringIO import StringIO
+except ImportError:
+    # Python 3.x
+    from io import StringIO
+    unicode = str
+    long = int
 
 from django.http import HttpResponse
 from django.db.models import Model

simplegetapi/utils.py
@@ -1,10 +1,20 @@
 import inspect
 
-from common import enum as enummodule
+if "unicode" not in globals():
+    # Python 3.x compatibility
+    unicode = str
+
+try:
+    from common import enum as enummodule
+    has_common_enum = True
+except:
+    has_common_enum = False
 
 from django.db.models.related import RelatedObject
 
 def is_enum(obj):
+    if not has_common_enum:
+        return False
     return inspect.isclass(obj) and issubclass(obj, enummodule.Enum)
     
 def get_orm_fields(obj):

simplegetapi/views.py
@@ -4,10 +4,10 @@ from django.db.models.fields.related import ForeignKey, ManyToManyField
 from django.db.models.related import RelatedObject
 from django.shortcuts import get_object_or_404
 from django.conf import settings
-import csv, json, StringIO, datetime, lxml, urllib
+import csv, json, datetime, lxml, urllib
 import dateutil.parser
 
-from utils import is_enum, get_orm_fields
+from simplegetapi.utils import is_enum, get_orm_fields
 from simplegetapi.serializers import serialize_object, serialize_response_json, serialize_response_jsonp, serialize_response_xml, serialize_response_csv
 
 def do_api_call(request, model, qs, id):
@@ -93,7 +93,14 @@ def do_api_search(model, qs, request_options, requested_fields):
     qs_sort = None
     qs_filters = { }
 
-    for arg, vals in request_options.iterlists():
+    try:
+        # Python 2.x
+        querystringargs = request_options.iterlists()
+    except:
+        # Python 3.x
+        querystringargs = request_options.lists()
+
+    for arg, vals in querystringargs:
         if arg in ("offset", "limit", "format", "fields", "callback"):
             # These aren't filters.
             pass
@@ -368,6 +375,9 @@ def build_api_documentation(model, qs):
             else:
                 field_info["filterable"] = "Filterable when also filtering on " + " and ".join(indexed_if[field_name]) + "."
                 
+        if "unicode" not in globals():
+            # Python 3.x compatibility
+            unicode = str
         if isinstance(field, (str, unicode)):
             # for api_additional_fields
             v = model.api_additional_fields[field] # get the attribute or function

commit e5a05a0387cc13e4cb662bdd7ff5e88fb8b2ba65

python 3x changes broke csv serialization in 4ed6eae5e695845e336632239b6c915ffe9ae1ac

simplegetapi/serializers.py
@@ -201,7 +201,7 @@ def serialize_response_csv(response, is_list, requested_fields, format):
         requested_fields.sort()
                 
     # write CSV to buffer
-    raw_data = StringIO.StringIO()
+    raw_data = StringIO()
     writer = csv.writer(raw_data)
     writer.writerow(requested_fields)
     def get_value_recursively(item, key):